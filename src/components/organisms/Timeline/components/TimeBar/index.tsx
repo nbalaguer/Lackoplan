b7c20aceaef43e865d00367fd3e76ed607fd02cb
Néstor Balaguer Adsuara <nbalaguer@ext.laliga.es>
2023-06-12 22:05:52 +0200

    Hide time guide when mouse exits panel

--- src/components/organisms/Timeline/components/TimeBar/index.tsx
+++ src/components/organisms/Timeline/components/TimeBar/index.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useAppStore } from "store"
 import { getCastTimes } from "utils"
 import useTimelineContext from "components/organisms/Timeline/context/useTimelineContext"
 import { motion, useSpring } from "framer-motion"
+import classNames from "classnames"
 
 function TimeBar() {
   const duration = useAppStore((state) => state.duration)
   const minutes = getCastTimes(60, duration)
+
+  const [isGuideVisible, setIsGuideVisible] = useState<boolean>(false)
 
   const { panelRef } = useTimelineContext()
   const x = useSpring(0, {
@@ -20,20 +23,35 @@ function TimeBar() {
     if (!panelRef.current) return
     const panel = panelRef.current
 
+    function handleEnter() {
+      setIsGuideVisible(true)
+    }
+
+    function handleLeave() {
+      setIsGuideVisible(false)
+    }
+
     function handleMove(event: MouseEvent) {
       x.set(event.clientX - panel.offsetLeft)
     }
 
     panel.addEventListener("mousemove", handleMove)
+    panel.addEventListener("mouseenter", handleEnter)
+    panel.addEventListener("mouseleave", handleLeave)
     return () => {
       panel.removeEventListener("mousemove", handleMove)
+      panel.removeEventListener("mouseenter", handleEnter)
+      panel.removeEventListener("mouseleave", handleLeave)
     }
   }, [panelRef, x])
 
   return (
     <div>
       <motion.div
-        className="absolute top-0 left-0 -z-10 h-full w-0 border-l-[1px] border-dashed border-slate-700/75"
+        className={classNames(
+          "absolute top-0 left-0 -z-10 h-full w-0 border-l-[1px] border-dashed border-slate-700/75 transition-opacity duration-100",
+          { ["opacity-0 duration-500"]: !isGuideVisible }
+        )}
         style={{ x }}
       />
       <div className="h-px bg-slate-500" />
