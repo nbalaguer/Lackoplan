a6e08b9d19f27de5e115383735cad3a851c371ea
Néstor Balaguer Adsuara <nbalaguer@ext.laliga.es>
2023-12-10 12:51:59 +0100

    Update Switch atom

--- src/components/atoms/Switch/index.tsx
+++ src/components/atoms/Switch/index.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 function Switch(props: {
   checked: boolean
   onChange: React.ChangeEventHandler
-  label: string
+  label?: string
   reverse?: boolean
 }) {
   const { checked, onChange, label, reverse } = props
@@ -18,10 +18,10 @@ function Switch(props: {
         onChange={onChange}
         checked={checked}
       />
-      {reverse && <span className="text-sm">{label}</span>}
+      {!!label && reverse && <span className="text-sm">{label}</span>}
       <div
         className={classNames(
-          "flex w-7 justify-start border-2 border-slate-500 bg-slate-700 transition-colors duration-100 group-hover:bg-slate-600",
+          "flex w-7 justify-start border-2 border-slate-500 bg-slate-500 transition-colors duration-100",
           {
             ["justify-end"]: checked,
           }
@@ -30,15 +30,10 @@ function Switch(props: {
         <motion.div
           layout
           transition={{ duration: 0.15 }}
-          className={classNames(
-            "h-2 w-3 bg-slate-500 transition-colors duration-150",
-            {
-              ["bg-slate-300"]: checked,
-            }
-          )}
+          className="h-2 w-3 bg-slate-700 transition-colors duration-150 group-hover:bg-slate-600"
         />
       </div>
-      {!reverse && <span className="text-sm">{label}</span>}
+      {!!label && !reverse && <span className="text-sm">{label}</span>}
     </label>
   )
 }
