e00536025ad90dcae63a607c859b8b3322acb8d1
Néstor Balaguer Adsuara <nbalaguer@ext.laliga.es>
2025-05-10 12:52:21 +0200

    Updated class selector UI. Notably, added class colors to quickly differentiate between classes isntead of having to decypher the small icons

--- src/components/organisms/LeftSidebar/components/ClassList/components/Class/index.tsx
+++ src/components/organisms/LeftSidebar/components/ClassList/components/Class/index.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { type CSSProperties } from "react"
 import WowheadIcon from "components/atoms/WowheadIcon"
 import { classIcons } from "config/classes"
 import { useAppStore } from "store"
 import type { Class } from "types"
 import { createPlayer } from "utils"
+import theme from "config/theme"
 
 function Class(props: { classKey: Class }) {
   const { classKey } = props
@@ -12,13 +13,16 @@ function Class(props: { classKey: Class }) {
 
   return (
     <button
-      className="flex outline outline-2 outline-transparent transition-[outline-color] duration-100 hover:outline-slate-500"
+      className="group flex transition-[outline-color] duration-100 rounded-full overflow-hidden border-4"
+      style={{
+        borderColor: theme.colors[classKey],
+      } as CSSProperties}
       onClick={() => {
         const newPlayer = createPlayer(classKey)
         addPlayer(newPlayer)
       }}
     >
-      <WowheadIcon name={classIcons[classKey]} size="large" />
+      <WowheadIcon name={classIcons[classKey]} size="medium" className="group-hover:scale-110 transition-transform" />
     </button>
   )
 }
--- src/components/organisms/LeftSidebar/components/ClassList/index.tsx
+++ src/components/organisms/LeftSidebar/components/ClassList/index.tsx
@@ -7,7 +7,7 @@ import WowheadIcon from "components/atoms/WowheadIcon"
 
 function ClassList() {
   return (
-    <div className="grid grid-cols-[repeat(5,auto)] gap-2 border-b-2 border-slate-700 p-4 px-10 justify-center">
+    <div className="grid grid-cols-[repeat(5,auto)] gap-2 border-b-2 border-slate-700 p-4 px-6 justify-center">
       {Object.values(CLASSES).map((classKey) => {
         const hasAbilities = !!abilities[classKey].length
         if (hasAbilities) return <Class key={classKey} classKey={classKey} />
